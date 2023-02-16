d85cf95e89ac2b53338be29f05099733cb6821fb
Devendradhote <83503966+Devendradhote@users.noreply.github.com>
2023-02-16 19:40:59 +0530

    rental services

--- js/script.js
+++ js/script.js
@@ -87,60 +87,97 @@ function consoleText(words, id, colors) {
   }, 400)
 }
 
-// const carousel = document.querySelector(".carousel"),
-// firstImg = carousel.querySelectorAll("img")[0],
-// arrowIcons = document.querySelectorAll(".wrapper i");
-// let isDragStart = false, isDragging = false, prevPageX, prevScrollLeft, positionDiff;
-// const showHideIcons = () => {
-//     let scrollWidth = carousel.scrollWidth - carousel.clientWidth;
-//     arrowIcons[0].style.display = carousel.scrollLeft == 0 ? "none" : "block";
-//     arrowIcons[1].style.display = carousel.scrollLeft == scrollWidth ? "none" : "block";
-// }
-// arrowIcons.forEach(icon => {
-//     icon.addEventListener("click", () => {
-//         let firstImgWidth = firstImg.clientWidth + 14;
-//         carousel.scrollLeft += icon.id == "left" ? -firstImgWidth : firstImgWidth;
-//         setTimeout(() => showHideIcons(), 60);
-//     });
-// });
-// const autoSlide = () => {
-//     if(carousel.scrollLeft - (carousel.scrollWidth - carousel.clientWidth) > -1 || carousel.scrollLeft <= 0) return;
-//     positionDiff = Math.abs(positionDiff);
-//     let firstImgWidth = firstImg.clientWidth + 14;
-//     let valDifference = firstImgWidth - positionDiff;
-//     if(carousel.scrollLeft > prevScrollLeft) { 
-//         return carousel.scrollLeft += positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff;
-//     }
-//     carousel.scrollLeft -= positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff;
-// }
-// const dragStart = (e) => {
-//     isDragStart = true;
-//     prevPageX = e.pageX || e.touches[0].pageX;
-//     prevScrollLeft = carousel.scrollLeft;
-// }
-// const dragging = (e) => {
-//     if(!isDragStart) return;
-//     e.preventDefault();
-//     isDragging = true;
-//     carousel.classList.add("dragging");
-//     positionDiff = (e.pageX || e.touches[0].pageX) - prevPageX;
-//     carousel.scrollLeft = prevScrollLeft - positionDiff;
-//     showHideIcons();
-// }
-// const dragStop = () => {
-//     isDragStart = false;
-//     carousel.classList.remove("dragging");
-//     if(!isDragging) return;
-//     isDragging = false;
-//     autoSlide();
-// }
-// carousel.addEventListener("mousedown", dragStart);
-// carousel.addEventListener("touchstart", dragStart);
-// document.addEventListener("mousemove", dragging);
-// carousel.addEventListener("touchmove", dragging);
-// document.addEventListener("mouseup", dragStop);
-// carousel.addEventListener("touchend", dragStop);
+var swiper = new Swiper(".home-slider", {
+  loop:true, 
+  grabCursor:true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
 
+var swiper = new Swiper(".product-slider", {
+  loop:true, 
+  grabCursor:true,
+  spaceBetween: 20,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      640: {
+        slidesPerView: 2,
+      },
+      768: {
+        slidesPerView: 3,
+      },
+      1024: {
+        slidesPerView: 4,
+      },
+  },
+});
 
+var swiper = new Swiper(".review-slider", {
+  loop:true, 
+  grabCursor:true,
+  spaceBetween: 20,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      640: {
+        slidesPerView: 2,
+      },
+      768: {
+        slidesPerView: 3,
+      },
+  },
+});
 
-// -----------------
+var swiper = new Swiper(".blogs-slider", {
+  loop:true, 
+  grabCursor:true,
+  spaceBetween: 10,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      991: {
+        slidesPerView: 3,
+      },
+  },
+});
+
+var swiper = new Swiper(".clients-slider", {
+  loop:true, 
+  grabCursor:true,
+  spaceBetween: 20,
+  breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      640: {
+        slidesPerView: 2,
+      },
+      768: {
+        slidesPerView: 3,
+      },
+      1024: {
+        slidesPerView: 4,
+      },
+  },
+});
